3c41fac2878eaf9f7c136bdca9c00be84e1cbf36
Carlos Diaz <cdiaz@bcnbit.com>
2020-03-08 09:30:49 +0100

    Updating project structure and deleting unused files.

--- src/data/global.js
+++ src/data/global.js
@@ -1,9 +1,12 @@
 module.exports = {
-    user_logged: false,
-    user_name: "",
-    user_role: "",
-    user_email: "",
+    user: {
+        logged: false,
+        name: "",
+        role: "",
+        email: ""
+    },
     msg: "",
+    section: "",
     api_base_url: "http://www.mocky.io/v2/",
     api_clients: "5808862710000087232b75ac",
     api_policies: "580891a4100000e8242b75c5",
